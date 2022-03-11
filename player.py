81c6718214e4fc1b5c8a462a59487fe1207ba13a
Russell M <russell.macalintal@gmail.com>
2022-03-10 22:03:09 -0500

    Added get_card method to Player class

--- player.py
+++ player.py
@@ -1,3 +1,5 @@
+import deck
+
 class Player:
 
     def __init__(self, name):
@@ -5,9 +7,19 @@ class Player:
         self.hand = []                  # List of cards that the player has on hand
 
     def __str__(self):
-        return f'Player {self.name} has the following cards: {"".join([str(x) for x in self.hand])}'
+        if self.hand == []:
+            return f'Player {self.name} has no cards'
+        else:
+            return f'Player {self.name} has the following cards: {", ".join([str(x) for x in self.hand])}'
+
+    def get_card(self, new_card):
+        self.hand.append(new_card)
 
 
 if __name__ == "__main__":
+    deck = deck.Deck()
+    deck.shuffle()
     player = Player('Stewart')
+    player.get_card(deck.deal_card())
+    player.get_card(deck.deal_card())
     print(player)
