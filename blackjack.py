9ed491e6e6f928702bf7c91482e7ddf717356694
Russell M <russell.macalintal@gmail.com>
2022-03-10 23:01:56 -0500

    Initiate blackjack.py game file

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,16 @@
 import deck
+import player
+import dealer
+
+player = player.Player('Jack Black')
+dealer = dealer.Dealer()
+new_deck = deck.Deck()
+new_deck.shuffle()
+
+player.get_card(new_deck.deal_card())
+player.get_card(new_deck.deal_card())
+
+dealer.get_card(new_deck.deal_card())       # Only give the dealer one card for now to avoid having to hide the second card - second card will be dealt and 'revealed' once the player has completed their turn
+
+print(player)
+print(dealer)
